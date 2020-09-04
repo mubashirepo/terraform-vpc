28b0ed54644d6f3a5ab2da75a9858daa84dcac5f
root <root@gmail.com>
2020-09-04 19:43:28 +0530

    vpc

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# terraform-vpc
+## Terraform Script to create VPC
+
+### 1. terraform init
+```bash
+terraform init
+```
+
+### 2. terraform plan
+```bash
+terraform plan
+```
+
+### 3. terraform apply
+```bash
+terraform apply
+```
---
+++ instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "web1" {
+    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    instance_type = "t2.micro"
+    # VPC
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+    # Security Group
+    vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+    # the Public SSH key
+    #key_name = "${aws_key_pair.london-region-key-pair.id}"
+    # nginx installation
+    provisioner "file" {
+        source = "nginx.sh"
+        destination = "/tmp/nginx.sh"
+    }
+    provisioner "remote-exec" {
+        inline = [
+             "chmod +x /tmp/nginx.sh",
+             "sudo /tmp/nginx.sh"
+        ]
+    }
+
+}
